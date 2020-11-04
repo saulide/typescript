9cdcb5f75256558d47c56d369462da7b7e83fdde
sarchipovas <kristalaz@gmail.com>
2020-11-04 08:07:54 +0000

    Adding ignore for code portion

--- models/Image.ts
+++ models/Image.ts
@@ -8,6 +8,7 @@ export class Image {
     constructor(attr: ImageProperties = {}) {
         // tslint:disable-next-line: forin
         for (const key in attr) {
+            // @ts-ignore
             this[key] = attr[key];
         }
     }
